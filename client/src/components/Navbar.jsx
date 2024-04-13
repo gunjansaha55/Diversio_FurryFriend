b0900987c299a4cc4ab9376ad2af6161cd452a23
GUNJAN SAHA <gunjansaha976@gmail.com>
2024-04-13 21:23:54 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
       <header>
         <div className="container">
           <div className="logo-brand">
-            <NavLink to="/">ThapaTechnical</NavLink>
+            <NavLink to="/">FurryFriends</NavLink>
           </div>
 
           <nav>
